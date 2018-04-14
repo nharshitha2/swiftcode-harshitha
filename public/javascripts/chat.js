3613e474ea1034af4ef545240899dc6946b685a8
nharshitha2 <nharshitha2@gmail.com>
2018-04-14 12:12:18 +0000

    View

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+	},
+        {
+
+            'sender': 'BOT',
+            'text': 'Hello'
+    },
+        {
+
+            'sender': 'USER',
+            'text': 'How are you'
+},
+        {
+
+            'sender': 'BOT',
+            'text': 'I am fine'
+}
+]
 
 
 });
